8f84a003ba66e2846f095edaf037c73023b9ae06
alfonsusrr <alfonsus737@gmail.com>
2024-10-07 07:27:10 +0800

    Fix .sh

--- eval/eval_nene.sh
+++ eval/eval_nene.sh
@@ -1,4 +1,9 @@
-read device
+if [ $# -eq 0 ]; then
+  echo "Please provide the device to use (e.g., cuda, cpu)"
+  exit 1
+fi
+
+device=$1
 
 export CUDA_VISIBLE_DEVICES=$device
 python3 eval.py --model nene-v1 --index_user nene-user --index_name ayachi-nene --character Ayachi Nene --embed_model mxbai-embed-large-v1 --modelfile_name ModelfileNene --eval_dataset nene.json --output_report nene-report.txt --device 2
--- inference.sh
+++ inference.sh
@@ -1,4 +1,9 @@
-read device
+if [ $# -eq 0 ]; then
+  echo "Please provide the device to use (e.g., cuda, cpu)"
+  exit 1
+fi
 
+device=$1
 export CUDA_VISIBLE_DEVICES=$device
+
 python3 inference3.py --model nene-v1 --index_user nene-user --index_name ayachi-nene --character Ayachi Nene --embed_model mxbai-embed-large-v1
