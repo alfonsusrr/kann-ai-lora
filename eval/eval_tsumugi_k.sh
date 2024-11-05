9f49fe9fcf8df407e9f4fab0d95f085586a72532
alfonsusrr <alfonsus737@gmail.com>
2024-11-05 23:22:23 +0800

    Add knowledge script

--- eval/eval_tsumugi_k.sh
+++ eval/eval_tsumugi_k.sh
@@ -6,4 +6,4 @@ fi
 device="cuda:GPU-f09680a9-291c-2a62-8f31-ea53013587d0"
 
 export CUDA_VISIBLE_DEVICES=GPU-f09680a9-291c-2a62-8f31-ea53013587d0
-python3 eval_fix.py --model tsumugi-v4 --index_user tsumugi-user --index_name shiiba-tsumugi --character Shiiba Tsumugi --embed_model mxbai-embed-large-v1 --modelfile_name ModelfileTsumugi --eval_dataset tsumugi_k.json --output_report tsumugi-report-knowledge.json --checkpoint tsumugi-knowledge-cpt.txt --device $device 
+python3 eval_fix.py --model tsumugi-v4 --index_user tsumugi-user --index_name shiiba-tsumugi --character Shiiba Tsumugi --embed_model mxbai-embed-large-v1 --modelfile_name ModelfileTsumugi --eval_dataset tsumugi_k.json --output_report tsumugi-report-knowledge.json --checkpoint tsumugi-knowledge-cpt.txt --know_eval True --device $device 
