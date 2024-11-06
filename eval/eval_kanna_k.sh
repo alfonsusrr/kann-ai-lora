44367b576ce726b7f191a793b55b5219e9803d95
alfonsusrr <alfonsus737@gmail.com>
2024-11-06 13:26:07 +0800

    Fix knowledge script

---
+++ eval/eval_kanna_k.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ]; then
+  echo "Please provide the device to use (e.g., cuda, cpu)"
+  exit 1
+fi
+
+device="cuda:GPU-f09680a9-291c-2a62-8f31-ea53013587d0"
+
+export CUDA_VISIBLE_DEVICES=GPU-f09680a9-291c-2a62-8f31-ea53013587d0
+python3 eval_fix.py --model kanna-v4 --index_user kanna-user --index_name akizuki-kanna --character Akizuki Kanna --embed_model mxbai-embed-large-v1 --modelfile_name ModelfileKanna --eval_dataset kanna_k.json --output_report kanna-report-knowledge.json --checkpoint kanna-knowledge-cpt.txt --know_eval True --device $device 
---
+++ eval/eval_natsume_k.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 0 ]; then
+  echo "Please provide the device to use (e.g., cuda, cpu)"
+  exit 1
+fi
+
+device="cuda:GPU-f09680a9-291c-2a62-8f31-ea53013587d0"
+
+export CUDA_VISIBLE_DEVICES=GPU-f09680a9-291c-2a62-8f31-ea53013587d0
+python3 eval_fix.py --model natsume-v4 --index_user natsume-user --index_name shiki-natsume --character Shiki Natsume --embed_model mxbai-embed-large-v1 --modelfile_name ModelfileNatsume --eval_dataset natsume_k.json --output_report natsume-report-knowledge.json --checkpoint natsume-knowledge-cpt.txt --know_eval True --device $device 
