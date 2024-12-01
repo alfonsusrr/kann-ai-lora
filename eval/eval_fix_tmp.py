613856680deeb22a78c4ba2b3049f7dcf491e127
Nicholas Sidharta <toxicancm@gmail.com>
2024-12-01 13:43:46 +0700

    try

--- eval/eval_fix_tmp.py
+++ eval/eval_fix_tmp.py
@@ -140,9 +140,9 @@ def handle_single_message(message_content, rag_prompt, args):
     global lora_model, tokenizer, embed_model, embed_tokenizer, index  
     
     appended_messages = [{
-        "from": "system",
-        "value": f"You are roleplaying a character that is named {' or '.join(args.character) if len(args.character) > 1 else args.character[0]}. Please provide a response that is engaging, in-character, and adds depth to the conversation. Make sure to be as detailed as possible. Do not include the character's name or any tags before the response. Only provide the spoken dialogue of the character you are roleplaying. \n" + 
-                    rag_prompt
+        "role": "system",
+        "content": f"You are roleplaying a character that is named {' or '.join(args.character) if len(args.character) > 1 else args.character[0]}. Please provide a response that is engaging, in-character, and adds depth to the conversation. Make sure to be as detailed as possible. Do not include the character's name or any tags before the response. Only provide the spoken dialogue of the character you are roleplaying.\n\n"
+                   "Note: You are having a conversation strictly with a human user. Any occurrence of personal pronouns such as 'I,' 'me,' 'my,' etc., should refer to the human user and not to anyone the character might have known before.\n"
     }] + message_content
 
     text = tokenizer.apply_chat_template(
@@ -174,8 +174,9 @@ def handle_single_message_no_rag(message_content, args):
     global lora_model, tokenizer
 
     appended_messages = [{
-        "from": "system",
-        "value": f"You are roleplaying a character that is named {' or '.join(args.character) if len(args.character) > 1 else args.character[0]}. Please provide a response that is engaging, in-character, and adds depth to the conversation. Make sure to be as detailed as possible. Do not include the character's name or any tags before the response. Only provide the spoken dialogue of the character you are roleplaying. \n"
+        "role": "system",
+        "content": f"You are roleplaying a character that is named {' or '.join(args.character) if len(args.character) > 1 else args.character[0]}. Please provide a response that is engaging, in-character, and adds depth to the conversation. Make sure to be as detailed as possible. Do not include the character's name or any tags before the response. Only provide the spoken dialogue of the character you are roleplaying.\n\n"
+                   "Note: You are having a conversation strictly with a human user. Any occurrence of personal pronouns such as 'I,' 'me,' 'my,' etc., should refer to the human user and not to anyone the character might have known before.\n"
     }] + message_content
 
     text = tokenizer.apply_chat_template(
